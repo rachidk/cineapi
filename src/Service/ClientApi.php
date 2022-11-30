09ce6a8a5102e2a908fd8f761f4837d5e8fee6cf
Rachid EL KHAMLICHI <elkhamlichi.ext@lemonde.fr>
2022-11-30 21:17:23 +0100

    add APi client

--- src/Service/ClientApi.php
+++ src/Service/ClientApi.php
@@ -3,13 +3,19 @@
 namespace App\Service;
 
 use App\Entity\Movie;
+use Doctrine\Persistence\ManagerRegistry;
+use Exception;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class ClientApi
 {
-    public function __construct(HttpClientInterface $client)
+    public const HTTP_STATUS_OK = 200;
+
+    private $manager;
+
+    public function __construct(HttpClientInterface $client, ManagerRegistry $manager)
     {
-        $this->client = $client;
+        $this->manager = $manager;
     }
 
     /**
@@ -17,22 +23,43 @@ class ClientApi
      * +met à jour l'entité et la persiste en bdd
      *
      */
-    public function getPosterFromMovie(Movie $movie)
+    public function getPosterFromMovie(Movie $movie): ?string
     {
-        $response = $this->client->request(
-            'GET',
-            'https://1mdb-data-searching.p.rapidapi.com/om',
-            [
-                'headers' => [
-                    'X-RapidAPI-Key' => '57a3a3f7b1mshe9df1b52c98bc95p1c6011jsn1ed2cc2d9d5a',
-                    'X-RapidAPI-Host' => '1mdb-data-searching.p.rapidapi.com',
-                ],
-                'query' => [
-                    't' => $movie->getTitle(),
-                ],
-            ]
-        );
-        $statusCode = $response->getStatusCode();
-        dd($statusCode);
+        try {
+            $response = $this->client->request(
+                'GET',
+                'https://movie-database-alternative.p.rapidapi.com/',
+                [
+                    'headers' => [
+                        'X-RapidAPI-Key' => '167f3c2aacmsh2c3a33f9a3c17fap1d983ajsnf53bdbaa3ffe',
+                        'X-RapidAPI-Host' => 'movie-database-alternative.p.rapidapi.com',
+                    ],
+                    'query' => [
+                        't' => $movie->getTitle(),
+                    ],
+                ]
+            );
+            if ($response->getStatusCode() == self::HTTP_STATUS_OK) {
+                $content = json_decode($response->getContent());
+
+                if ($content->Response == 'False') {
+                    return null;
+                }
+
+                $movie->setPoster($content->Poster);
+                $this->updatePosterMovie($movie);
+            }
+
+            return $content->Poster;
+        } catch (Exception $ex) {
+            return null;
+        }
+    }
+
+    private function updatePosterMovie(Movie $movie)
+    {
+        $manager = $this->manager->getManager();
+        $manager->persist($movie);
+        $manager->flush();
     }
 }
